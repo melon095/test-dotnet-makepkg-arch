8b83c30530219868fb6095e6019644ccd95daa6d
Joachim Flottorp <joachim@flottorp.org>
2025-03-09 00:19:14 +0100

    42

--- Program.cs
+++ Program.cs
@@ -1,3 +1,3 @@
 ﻿Console.WriteLine("The Industrial Revolution and its consequences have been a disaster for the human race.");
 Console.WriteLine("This is a new change :OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
-Console.WriteLine(1+1);
+Console.WriteLine(21+21);
